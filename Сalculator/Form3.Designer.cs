362ceb63cbd7ee7d8246d727409652cf9dffef83
Nikita <iglin.95@mail.ru>
2018-04-22 23:44:39 +0500

    меньше поле больше шрифт

--- Сalculator/Form3.Designer.cs
+++ Сalculator/Form3.Designer.cs
@@ -55,10 +55,11 @@
             // 
             // textBox1
             // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBox1.Location = new System.Drawing.Point(94, 49);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 50);
+            this.textBox1.Size = new System.Drawing.Size(100, 28);
             this.textBox1.TabIndex = 3;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
